62d2f0844d204f9548efc1eb5516afd74b07fd58
Joel Betterly <roninmoto@gmail.com>
2012-02-01 12:02:11 -0500

    Added 20 items to list

--- MIU/Joel_Betterly_Project1/js/JSON.js
+++ MIU/Joel_Betterly_Project1/js/JSON.js
@@ -2,11 +2,11 @@ var json = {
     "appt1":{
         "group": ["Selection:", "Personal"],
         "range": ["Level of Importance", "10"],
-        "name": ["Name:", "Jose Bandersmit"],
+        "name": ["Name:", "lio"],
         "date": ["Date:", "01-01-2012"],
         "tom": ["Time of Meeting:", "1200"],
         "available": ["Available:", "Yes"],
-        "notes": ["Notes:", "This is a test note."]
+        "notes": ["Notes:", "Hire a translator for meeting."]
     },
     "appt2":{
         "group": ["Selection:", "Business"],
@@ -25,5 +25,158 @@ var json = {
         "tom": ["Time of Meeting:", "1600"],
         "available": ["Available:", "Yes"],
         "notes": ["Notes:", "Big Macs for everyone!"]
-    }    
+    },
+    "appt4":{
+        "group": ["Selection:", "Personal"],
+        "range": ["Level of Importance", "8"],
+        "name": ["Name:", "Daffy Duck"],
+        "date": ["Date:", "01-12-2012"],
+        "tom": ["Time of Meeting:", "1300"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "No mocking his speech impediment."]
+    },
+    "appt5":{
+        "group": ["Selection:", "Personal"],
+        "range": ["Level of Importance", "6"],
+        "name": ["Name:", "Cookie Crook"],
+        "date": ["Date:", "01-12-2012"],
+        "tom": ["Time of Meeting:", "1800"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Lock up cereal before he arrives."]
+    },
+    "appt6":{
+        "group": ["Selection:", "Personal"],
+        "range": ["Level of Importance", "7"],
+        "name": ["Name:", "Bugs Bunny"],
+        "date": ["Date:", "02-01-2012"],
+        "tom": ["Time of Meeting:", "1700"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Requested meeting in hole by highway."]
+    },
+    "appt7":{
+        "group": ["Selection:", "Personal"],
+        "range": ["Level of Importance", "9"],
+        "name": ["Name:", "Family Circle"],
+        "date": ["Date:", "02-10-2012"],
+        "tom": ["Time of Meeting:", "800"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Discuss Jeffies intervention; something about sniffing Elmer's glue."]
+    },
+    "appt8":{
+        "group": ["Selection:", "Personal"],
+        "range": ["Level of Importance", "3"],
+        "name": ["Name:", "Sluggo"],
+        "date": ["Date:", "01-19-2012"],
+        "tom": ["Time of Meeting:", "1430"],
+        "available": ["Available:", "no"],
+        "notes": ["Notes:", "Court appearance for domestic abuse."]
+    },
+    "appt9":{
+        "group": ["Selection:", "Business"],
+        "range": ["Level of Importance", "10"],
+        "name": ["Name:", "Clark Kent"],
+        "date": ["Date:", "01-16-2012"],
+        "tom": ["Time of Meeting:", "1845"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Meeting in basement of Daily Planet."]
+    },
+    "appt10":{
+        "group": ["Selection:", "Business"],
+        "range": ["Level of Importance", "1"],
+        "name": ["Name:", "Superman"],
+        "date": ["Date:", "01-16-2012"],
+        "tom": ["Time of Meeting:", "1845"],
+        "available": ["Available:", "no"],
+        "notes": ["Notes:", "Prior meeting with Clark Kent, will reschedule."]
+    },
+    "appt11":{
+        "group": ["Selection:", "Business"],
+        "range": ["Level of Importance", "5"],
+        "name": ["Name:", "Sponge Bob Square Pants"],
+        "date": ["Date:", "01-23-2012"],
+        "tom": ["Time of Meeting:", "900"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Bring Scuba gear, meeting in pineapple under the sea."]
+    },
+    "appt12":{
+        "group": ["Selection:", "Business"],
+        "range": ["Level of Importance", "6"],
+        "name": ["Name:", "Johhny Quest"],
+        "date": ["Date:", "01-24-2012"],
+        "tom": ["Time of Meeting:", "1200"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Sueing Hadji for quiting show before end of contract."]
+    },
+    "appt13":{
+        "group": ["Selection:", "Business"],
+        "range": ["Level of Importance", "7"],
+        "name": ["Name:", "GI Joe"],
+        "date": ["Date:", "01-17-2012"],
+        "tom": ["Time of Meeting:", "1200"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Wants to join Air Force, needs PR plan."]
+    },
+    "appt14":{
+        "group": ["Selection:", "Business"],
+        "range": ["Level of Importance", "8"],
+        "name": ["Name:", "Lion-O"],
+        "date": ["Date:", "01-25-2012"],
+        "tom": ["Time of Meeting:", "1700"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Lost Sword of Omens, wants to know if he can legally blame Mumm-Ra."]
+    },
+    "appt15":{
+        "group": ["Selection:", "Other"],
+        "range": ["Level of Importance", "9"],
+        "name": ["Name:", "He-Man"],
+        "date": ["Date:", "02-20-2012"],
+        "tom": ["Time of Meeting:", "2000"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Wedding plans to She-Ra. Cringer is objecting."]
+    },
+    "appt16":{
+        "group": ["Selection:", "Business"],
+        "range": ["Level of Importance", "10"],
+        "name": ["Name:", "Burger King"],
+        "date": ["Date:", "03-01-2012"],
+        "tom": ["Time of Meeting:", "1200"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Discuss plans to overthrow Ronald McDonald in coo attempt."]
+    },
+    "appt17":{
+        "group": ["Selection:", "Business"],
+        "range": ["Level of Importance", "7"],
+        "name": ["Name:", "Buckey Cat"],
+        "date": ["Date:", "03-11-2012"],
+        "tom": ["Time of Meeting:", "1200 - 1211"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "New book idea, Ferrets for Dummies, how to kill them."]
+    },
+    "appt18":{
+        "group": ["Selection:", "Other"],
+        "range": ["Level of Importance", "9"],
+        "name": ["Name:", "Jessica Rabbit"],
+        "date": ["Date:", "02-18-2012"],
+        "tom": ["Time of Meeting:", "900"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Divorce settlement on Rodger Rabbit."]
+    },
+    "appt19":{
+        "group": ["Selection:", "Other"],
+        "range": ["Level of Importance", "1"],
+        "name": ["Name:", "Bevis and Butt Head"],
+        "date": ["Date:", "02-11-2012"],
+        "tom": ["Time of Meeting:", "1947"],
+        "available": ["Available:", "no"],
+        "notes": ["Notes:", "Some butt head idea about getting chicks."]
+    },
+    "appt20":{
+        "group": ["Selection:", "Business"],
+        "range": ["Level of Importance", "10"],
+        "name": ["Name:", "Wild E Coyote"],
+        "date": ["Date:", "03-16-2012"],
+        "tom": ["Time of Meeting:", "1300"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Bringing lawsuit against ACME company for fraud of purcahsed products."]
+    }        
 }
