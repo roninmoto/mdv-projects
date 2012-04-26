2b8ec3890e88a6b0db61146b5732dc89909187d2
Joel Betterly <roninmoto@gmail.com>
2012-04-26 19:37:28 -0400

    Had to add a name tag to i could generate a key for editing, but couldn't figure out how to complete project.  Taking the zero.

--- ASD/Joel_Betterly_Project_4A_ASD/appt1/app/_attachments/main.js
+++ ASD/Joel_Betterly_Project_4A_ASD/appt1/app/_attachments/main.js
@@ -5,10 +5,9 @@ $(document).ready(function(){
 	$('#submit').on('click', function(x){
 		x.preventDefault();
 		var savecdb = {};
-		var dt = new Date();
-		var key = (dt.getTime());
-		savecdb._id = "appt:"+ key + $('#toa').val();
-		savecdb.toa = $("#toa").val();
+		savecdb._id = "appt:"+ $('#name').val();
+		savecdb.name = $('#name').val();
+		savecdb.toa = $('#toa').val();
 		savecdb.loi = $('#loi').val();
 		savecdb.notes = $('#notes').val();
 		$database.saveDoc(savecdb, {
@@ -34,18 +33,20 @@ $(document).ready(function(){
 			success : function(data){
 				$.each(data.rows, function(index, appt){
 					var id = "appt:" + appt.value.name;
+					var name = appt.value.name;
 					var toa = appt.value.toa;
 					var loi = appt.value.loi;
 					var notes = appt.value.notes;
 					$(
 						'<div class="Appointments">' +	
 							'<ul data-role="listview">' +
+							    '<li>Name of Appointment: '+ name +'</li>' +
 								'<li>Type of Appointment: '+ toa +'</li>' +
 								'<li>Level of Importantance: '+ loi +'</li>' +
 								'<li>Notes: '+ notes +'</li>' +
-								'<a href="#" class="edit" id ="'+ id +'" >Edit Appointment</a>' +
+								'<a href="#" id ="'+ id +'" >Edit Appointment</a>' +
 								'<br />' +
-								'<a href="#" class="delete" id ="'+ id +'"> Delete Appointment</a>' +
+								'<a href="#" id ="'+ id +'"> Delete Appointment</a>' +
 							'</ul>' +
 						'</div>' 
 					)
@@ -54,6 +55,27 @@ $(document).ready(function(){
 			}
 		});
 	});	
-	
 
+	$('div#DOMin').click(function(event){
+		var id = "appt:" + appt.value.name;
+	$database.openDoc(id, {
+		
+		success: function(doc){
+			var name = doc.name;
+			var toa = doc.toa;
+			var loi = doc.loi;
+			var notes = doc.notes;
+			$database.removeDoc(doc);
+			$.mobile.changePage('#addItemsForm');
+			$('#name').val(name);
+			$('#toa').val(toa);
+			$('#loi').val(loi);
+			$('#notes').val(notes);
+
+		}
+	})	
+	});
+
+	
+	
 }); //Closing bracket for document ready
--- ASD/Joel_Betterly_Project_4A_ASD/appt1/app/views/appt1/map.js
+++ ASD/Joel_Betterly_Project_4A_ASD/appt1/app/views/appt1/map.js
@@ -1,6 +1,7 @@
 function (doc){
 	if (doc._id.substr(0,4) === "appt") {
 		emit(doc._id,{
+			"name": doc.name,
 			"toa": doc.toa,
 			"loi": doc.loi,
 			"notes": doc.notes
