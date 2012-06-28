a1d6c9a6bc859e558d943fc4779e1d87975a3561
roninmoto <roninmoto@gmail.com>
2012-06-28 11:11:22 -0400

    First Upload to git to establish product.

---
+++ AOC1/AOC_project1/AOC_project1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC_project1
+//
+//  Created by Joel Betterly on 6/28/12.
+//  Copyright (c) 2012 Full Sail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC1/AOC_project1/AOC_project1Tests/AOC_project1Tests.m
@@ -0,0 +1,32 @@
+//
+//  AOC_project1Tests.m
+//  AOC_project1Tests
+//
+//  Created by Joel Betterly on 6/28/12.
+//  Copyright (c) 2012 Full Sail. All rights reserved.
+//
+
+#import "AOC_project1Tests.h"
+
+@implementation AOC_project1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AOC_project1Tests");
+}
+
+@end
