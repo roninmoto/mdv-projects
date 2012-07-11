e8e08c76b3d09d36dbdb1bed418b5628a81744fb
roninmoto <roninmoto@gmail.com>
2012-07-11 14:05:24 -0400

    Start of Project 2 AOC1 Term 0712

---
+++ AOC1/AOC1_project2/AOC1_Project2/AOC1_Project2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC1_Project2
+//
+//  Created by Joel Betterly on 7/11/12.
+//  Copyright (c) 2012 Full Sail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
