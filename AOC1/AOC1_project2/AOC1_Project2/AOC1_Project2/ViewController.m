b5fb6793bfff5cca423eaa4141792f28ccff9967
roninmoto <roninmoto@gmail.com>
2012-07-11 14:37:33 -0400

    created button for the titleOfBook (The Stand).

--- AOC1/AOC1_project2/AOC1_Project2/AOC1_Project2/ViewController.m
+++ AOC1/AOC1_project2/AOC1_Project2/AOC1_Project2/ViewController.m
@@ -17,11 +17,22 @@
 - (void)viewDidLoad
 //Add all code here for buttons
 
-//Step 1 Change background color.
+//Step 1 [View] Change background color.
 {
     self.view.backgroundColor = [UIColor brownColor];
 
-
+//Step 1 [Book Information and UILabels] create a UILabel and place it at the top. Center the text.
+//Make sure to add variables to the ViewController.h file for each variable/button you make.
+    
+    titleOfBook = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 40)];
+    if (titleOfBook !=nil) {
+        titleOfBook.text = @"The Stand";
+        titleOfBook.textColor = [UIColor whiteColor];
+        titleOfBook.backgroundColor = [UIColor blueColor];
+        titleOfBook.textAlignment = UITextAlignmentCenter;
+    }
+    [self.view addSubview:titleOfBook];
+    
 
 
 
