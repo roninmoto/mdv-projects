a8226e94a933119301834505147ca2f3493d92c6
roninmoto <roninmoto@gmail.com>
2012-07-11 19:05:08 -0400

    Created NSArray, NSMutableString.  Looped through array and created output.  Created a number of variable types *stuff *mutablestring etc.  Tested loop and it works!  Now have to complete color changes on items and project is complete.

--- AOC1/AOC1_project2/AOC1_Project2/AOC1_Project2/ViewController.m
+++ AOC1/AOC1_project2/AOC1_Project2/AOC1_Project2/ViewController.m
@@ -17,11 +17,13 @@
 - (void)viewDidLoad
 //Add all code here for buttons
 
-//Step 1 [View] Change background color.
+//Section I [View] 
+//Step 1 Change background color.
 {
     self.view.backgroundColor = [UIColor brownColor];
 
-//Step 1 [Book Information and UILabels] create a UILabel and place it at the top. Center the text.
+//Section II [Book Information and UILabels]   
+//Step 1 Create a UILabel and place it at the top. Center the text.
 //Make sure to add variables to the ViewController.h file for each variable/button you make.
     
     titleOfBook = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 40)];
@@ -60,7 +62,7 @@
 //Step 4 Create a uilabel on next line called "Published: " Text right justified.
 //Copy/paste step two change variable and colors
     
-    published = [[UILabel alloc] initWithFrame:CGRectMake(10, 110, 100, 25)];    
+    published = [[UILabel alloc] initWithFrame:CGRectMake(10, 103, 100, 25)];    
     if (published !=nil) {
         published.text = @"Published: ";
         published.textColor = [UIColor whiteColor];
@@ -71,7 +73,7 @@
 //Step 5 Create a label next to the published label and add the text of when the book was publisjed, left justify text.
 //Copy/Paste, change variable and colors and coordinates just like above.
 
-    publishedDate = [[UILabel alloc] initWithFrame:CGRectMake(115, 110, 200, 25)];    
+    publishedDate = [[UILabel alloc] initWithFrame:CGRectMake(115, 103, 200, 25)];    
     if (publishedDate !=nil) {
         publishedDate.text = @"September 1978";
         publishedDate.textColor = [UIColor whiteColor];
@@ -83,7 +85,7 @@
 //Step 6 Create a UILabel with the text "Summary".  Left justify text.
 //You guessed it, copy and paste blah blah blah.
     
-    summary = [[UILabel alloc] initWithFrame:CGRectMake(10, 138, 100, 25)];    
+    summary = [[UILabel alloc] initWithFrame:CGRectMake(10, 130, 100, 25)];    
     if (summary !=nil) {
         summary.text = @"Summary";
         summary.textColor = [UIColor whiteColor];
@@ -95,7 +97,7 @@
 //Step 7 Create another UILabel that contains a small summary of the books plot. Text is centered span multiple lines.
 //Copy/paste blah, blah blah, make sure to add numberoflines = 9 (whatever you need) align text.
     
-    plot = [[UILabel alloc] initWithFrame:CGRectMake(10, 162, 300, 200)];    
+    plot = [[UILabel alloc] initWithFrame:CGRectMake(10, 155, 300, 200)];    
     if (plot !=nil) {
         plot.text = @"A devastating flu escapes from its containment in a military biowarfare lab, killing most of the population of the world. Those who remain, immune to the disease, receive dreams that lead them to align themselves either with the forces of good, or the forces of evil, prepare for the inevitable confrontation between the two sides.";
         
@@ -104,10 +106,53 @@
         plot.textColor = [UIColor whiteColor];
         plot.backgroundColor = [UIColor blueColor];
         plot.textAlignment = UITextAlignmentCenter;
-        plot.numberOfLines = 10;
+        plot.numberOfLines = 9;
     }
     [self.view addSubview:plot];     
     
+//Section III [Labels and Arrays]
+    
+//Step 1 Create an NSArray, 5 items, Items to be NSStrings Add items to array.
+    
+    NSArray *bookItemsArray = [[NSArray alloc] initWithObjects:@"Walking Dude", @"Captain Trips", @"Trashcan Man ", @"Boulder Colorado", @"Kojack", nil];
+    
+//Step 2 Create a variable type NSMuatableString, alloc  loop through NSArray append to the string seperated by commas.
+    
+    NSMutableString *mutableString = [[NSMutableString alloc] init];
+    for (int i=0; i<bookItemsArray.count; i++) 
+    {NSString *stuff = [bookItemsArray objectAtIndex:i];
+        if (bookItemsArray.count == i+1){
+            
+            [mutableString appendString:stuff];
+            
+        } else {
+            
+            [mutableString appendString:[[NSString alloc] initWithFormat:@"%@, ", stuff]];
+        }
+    }
+    
+//Step 3 Create a label with the "List Of Items" add to parent view, text left justified.
+    
+    itemsList = [[UILabel alloc] initWithFrame:CGRectMake(10, 360, 130, 25)];    
+    if (itemsList !=nil) {
+        itemsList.text = @"List of Items: ";
+        itemsList.textColor = [UIColor whiteColor];
+        itemsList.backgroundColor = [UIColor blueColor];
+        itemsList.textAlignment = UITextAlignmentLeft;
+    }
+    [self.view addSubview:itemsList];   
+    
+//Step 4 create a label set the text nsmutablestring text. increse lines text is cenetered.
+    
+    arrayOutput = [[UILabel alloc] initWithFrame:CGRectMake(10, 385, 300, 100)];    
+    if (arrayOutput !=nil) {
+        arrayOutput.text = mutableString;
+        arrayOutput.textColor = [UIColor whiteColor];
+        arrayOutput.backgroundColor = [UIColor blueColor];
+        arrayOutput.textAlignment = UITextAlignmentLeft;
+        arrayOutput.numberOfLines = 9;
+    }
+    [self.view addSubview:arrayOutput];     
     
     
     
