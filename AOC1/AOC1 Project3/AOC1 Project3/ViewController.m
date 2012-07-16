d3f452bfac07299435698e2e8503adb682981c00
roninmoto <roninmoto@gmail.com>
2012-07-16 10:56:31 -0400

    built the add function with my variables and then output the info to nslog file.

--- AOC1/AOC1 Project3/AOC1 Project3/ViewController.m
+++ AOC1/AOC1 Project3/AOC1 Project3/ViewController.m
@@ -20,6 +20,22 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated{
+    
+//1. Create a function called Add.  This function will take two NSInterger values an return the result of an addition between them.
+    
+    NSInteger addFunc = [self firstNum:2 secondNum:3];
+    NSLog(@"The result of adding two numbers together is %d.",addFunc);
+}
+
+
+//1. The Add Function
+
+-(int)firstNum:(int)num1 secondNum:(int)num2{
+    return num1 + num2;
+}
+
+
 - (void)viewDidUnload
 {
     [super viewDidUnload];
