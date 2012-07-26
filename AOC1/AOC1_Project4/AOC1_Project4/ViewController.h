3fbff1f0902c71532c8fa29906810bd07b24ebe9
roninmoto <roninmoto@gmail.com>
2012-07-26 09:09:01 -0400

    inital start of project.

---
+++ AOC1/AOC1_Project4/AOC1_Project4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC1_Project4
+//
+//  Created by Joel Betterly on 7/26/12.
+//  Copyright (c) 2012 Full Sail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AOC1/AOC1_Project4/AOC1_Project4Tests/AOC1_Project4Tests.m
@@ -0,0 +1,32 @@
+//
+//  AOC1_Project4Tests.m
+//  AOC1_Project4Tests
+//
+//  Created by Joel Betterly on 7/26/12.
+//  Copyright (c) 2012 Full Sail. All rights reserved.
+//
+
+#import "AOC1_Project4Tests.h"
+
+@implementation AOC1_Project4Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AOC1_Project4Tests");
+}
+
+@end
