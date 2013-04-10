3baae3a0b64e533e3bbc1838b03fa745824e98f2
Joel Betterly <roninmoto@gmail.com>
2013-04-10 13:37:09 -0400

    Start of Project. Initalized project and tracking all files for project.

---
+++ Java1/Week 1/gen/com/joelbetterly/java1week1/BuildConfig.java
@@ -0,0 +1,11 @@
+/*
+ * Project: Java1Week1
+ * Package: com.joelbetterly.java1week1
+ * @author: Joel Betterly
+ * Date: 	Apr 10, 2013
+ */
+package com.joelbetterly.java1week1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Java1/Week 1/src/com/joelbetterly/java1week1/MainActivity.java
@@ -0,0 +1,40 @@
+/*
+ * Project: Java1Week1
+ * Package: com.joelbetterly.java1week1
+ * @author: Joel Betterly
+ * Date: 	Apr 10, 2013
+ */
+package com.joelbetterly.java1week1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity
+{
+
+	LinearLayout ll;
+	LinearLayout.LayoutParams lp;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		//setup the linear layout
+		ll = new LinearLayout(this);
+		ll.setOrientation(LinearLayout.VERTICAL);
+		lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+		ll.setLayoutParams(lp);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
