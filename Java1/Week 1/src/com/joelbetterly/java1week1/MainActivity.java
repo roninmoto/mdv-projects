2d04618fb9f79546661ca368d2bed723e6352a51
Joel Betterly <roninmoto@gmail.com>
2013-04-10 18:00:46 -0400

    Added my Int(s) so it will pull from the resource file.  Also created int to pull value from field when tip amount is entered.  Created doubles so that when tips get paid, each group makes cetain amount.

--- Java1/Week 1/src/com/joelbetterly/java1week1/MainActivity.java
+++ Java1/Week 1/src/com/joelbetterly/java1week1/MainActivity.java
@@ -10,13 +10,24 @@ package com.joelbetterly.java1week1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Button;
+
+
+
 
 public class MainActivity extends Activity
 {
 
+	//Global Variables
 	LinearLayout ll;
 	LinearLayout.LayoutParams lp;
+	EditText et;
+	
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -28,6 +39,32 @@ public class MainActivity extends Activity
 		ll.setOrientation(LinearLayout.VERTICAL);
 		lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
 		ll.setLayoutParams(lp);
+		
+		//Add the TextView
+		TextView tv = new TextView(this);
+		tv.setText("% based on Tips");
+		ll.addView(et);
+		
+		//Add edit text for click event
+		et = new EditText(this);
+		et.setHint("Tip Amount");
+		
+		//Add Button for calculation
+		Button b = new Button(this);
+		b.setText("Get Tip");
+		b.setOnClickListener(new View.OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				// Setup Integers for each group
+				int adult = getResources().getInteger(R.integer.adult);
+				int teen = getResources().getInteger(R.integer.teen);
+				int kid = getResources().getInteger(R.integer.kid);
+				int tot = getResources().getInteger(R.integer.tot);
+			}
+		});
 	}
 
 	@Override
