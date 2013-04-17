1fbf80658cad4bd929bb8d64b6c12686f4093eb6
Joel Betterly <roninmoto@gmail.com>
2013-04-17 11:50:01 -0400

    intial upload for week2 project..

---
+++ Java1/Week 2/gen/com/jbetterly/automobile/BuildConfig.java
@@ -0,0 +1,11 @@
+/*
+ * Project: automobile
+ * Package: com.jbetterly.automobile
+ * @author: Joel Betterly
+ * Date: 	Apr 17, 2013
+ */
+package com.jbetterly.automobile;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Java1/Week 2/src/com/jbetterly/automobile/MainActivity.java
@@ -0,0 +1,27 @@
+/*
+ * Project: automobile
+ * Package: com.jbetterly.automobile
+ * @author: Joel Betterly
+ * Date: 	Apr 17, 2013
+ * Hoping to use http://developer.edmunds.com/docs/The_Vehicle_API/ for API
+ */
+
+package com.jbetterly.automobile;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+	}
+
+
+
+}
