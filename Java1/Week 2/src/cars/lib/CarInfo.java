3a554b75a8f261b63f33c23fad311fdd70ffd369
Joel Betterly <roninmoto@gmail.com>
2013-04-17 13:05:34 -0400

    new class for car data to output to string

---
+++ Java1/Week 2/src/cars/lib/CarInfo.java
@@ -0,0 +1,18 @@
+package cars.lib;
+
+public class CarInfo
+{
+	public String name;
+	public String make;
+	public String price;
+	public String madeIn;
+	
+	public CarInfo(String carName, String carMake, String carPrice, String carMadeIn)
+	{
+		name = carName;
+		make = carMake;
+		price = carPrice;
+		madeIn = carMadeIn;
+				
+	}
+}
