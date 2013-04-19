22b4b190ff0c97690c0772cfcf91b769366503a9
Joel Betterly <roninmoto@gmail.com>
2013-04-18 21:19:52 -0400

    added comments and fixed small issues

--- Java1/Week 2/src/com/jbetterly/automobile/MainActivity.java
+++ Java1/Week 2/src/com/jbetterly/automobile/MainActivity.java
@@ -54,26 +54,28 @@ public class MainActivity extends Activity
 			public void onClick(View v) {
 				
 				EditText carSearch = (EditText) v.getTag();
-				if(carSearch.getText().toString().length() > 0)   // CONDITIONAL - Car Search empty
+				// CONDITIONAL - Car Search empty per requirement
+				if(carSearch.getText().toString().length() > 0)   
 				{
-					Log.i("Go Button Action: ", carSearch.getText().toString());  //log out a message
+					//log out a message
+					Log.i("Go Button Action: ", carSearch.getText().toString());  
 				
 				}else{
+					
 					String logstuff = "Please enter text";
+					//log out a message
 					Log.i("Go Button Action: ", logstuff);
 				}
 			}
 		});
 		
         //Using my interface to add to my CarList - all static for now. 
-        //pg 132 in class book -  Learn Java for Android Development
-        
         ArrayList<Cars> cars = new ArrayList<Cars>();
         cars.add(new CarList("BMW", "Mini Cooper"));
         cars.add(new CarList("Jeep", "Wrangler"));
         cars.add(new CarList("Chrysler", "Pacifica"));
         
-        // need to find a better way, but this works for now...
+        // For loop to log out to String
         for (Cars data : cars) 
         {
             Log.i("New cars ",data.toString());
@@ -83,21 +85,69 @@ public class MainActivity extends Activity
         ((LinearLayout) myll).addView(ll);
 		
         //Static data for xml import.
-        CarInfo Car1 = new CarInfo(getString(R.string.usa_name), getString(R.string.usa_make), getString(R.string.usa_price), MadeIn.HOMEMADE.toString());
-        CarInfo Car2 = new CarInfo(getString(R.string.usa2_name), getString(R.string.usa2_make), getString(R.string.usa2_price), MadeIn.HOMEMADE.toString());
-        CarInfo Car3 = new CarInfo(getString(R.string.usa3_name), getString(R.string.usa3_make), getString(R.string.usa3_price), MadeIn.HOMEMADE.toString());
-        CarInfo Car4 = new CarInfo(getString(R.string.intntl_name), getString(R.string.intntl_make), getString(R.string.intntl_price), MadeIn.CHINAMADE.toString());
-        CarInfo Car5 = new CarInfo(getString(R.string.intntl2_name), getString(R.string.intntl2_make), getString(R.string.intntl2_price), MadeIn.CHINAMADE.toString());
-	}
+        CarInfo car1 = new CarInfo(getString(R.string.usa_name), getString(R.string.usa_make), getString(R.string.usa_price), MadeIn.HOMEMADE.toString());
+        CarInfo car2 = new CarInfo(getString(R.string.usa2_name), getString(R.string.usa2_make), getString(R.string.usa2_price), MadeIn.HOMEMADE.toString());
+        CarInfo car3 = new CarInfo(getString(R.string.usa3_name), getString(R.string.usa3_make), getString(R.string.usa3_price), MadeIn.HOMEMADE.toString());
+        CarInfo car4 = new CarInfo(getString(R.string.intntl_name), getString(R.string.intntl_make), getString(R.string.intntl_price), MadeIn.CHINAMADE.toString());
+        CarInfo car5 = new CarInfo(getString(R.string.intntl2_name), getString(R.string.intntl2_make), getString(R.string.intntl2_price), MadeIn.CHINAMADE.toString());
+	
 
+	// setting up array using CarInfo class
+    final CarInfo[] Auto = { car1, car2, car3, car4, car5 };
+    
+    // setting up array using car names
+    String[] AutoNames = { car1.name, car2.name, car3.name, car4.name, car5.name };
+    
+    // Calling my list view... 
+    theCarList = (ListView) findViewById(R.id.theCarList);
+    
+    // Created ArrayList Names
+    ArrayList<String> allCarNamesList = new ArrayList<String>();  
+    allCarNamesList.addAll( Arrays.asList(AutoNames) );
+    
+    // Created ArrayAdapter 
+    listAdapter = new ArrayAdapter<String>(this, R.layout.row, allCarNamesList); 
+    
+    // Set the ArrayAdapter   
+    theCarList.setAdapter(listAdapter);
 
-
+	
+	
+    theCarList.setOnItemClickListener(new OnItemClickListener() 
+    {
+		@Override
+		public void onItemClick(AdapterView<?> arg0,
+				View theListAdapter, int arg2, long arg3) 
+				{
+			TextView s = (TextView) theListAdapter;
+			
+			// UI Output 
+			Log.i("Name: ", s.getText().toString());
+			String chosenCar = s.getText().toString();
+			for(int i=0; i<Auto.length; i++)
+        {
+            	CarInfo carStr = Auto[i];
+            	if (chosenCar == carStr.name) 
+            	{
+            		String carPrice = carStr.price;
+            		//logging output when row is clicked
+            		Log.i("Price: ", carPrice);
+            		
+            		String carMake = carStr.make;
+            		//logging output when row is clicked
+            		Log.i("Make: ", carMake);
+            		
+            		String carMadeIn = carStr.madeIn;
+            		//logging output when row is clicked
+            		Log.i("Type: ", carMadeIn);
+            	} 
+        }
+			
+      }
+    });
 	
 	
 	
-	
-	
-	
-	
+	}	
 	
 }
