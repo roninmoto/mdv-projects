5b1dfa032984d779448fa8b39663dd090c57f3cd
Joel Betterly <roninmoto@gmail.com>
2013-04-25 12:22:13 -0400

    added globals for VIN

--- Java1/Week 3/src/com/jbetterly/auto2/MainActivity.java
+++ Java1/Week 3/src/com/jbetterly/auto2/MainActivity.java
@@ -2,26 +2,34 @@ package com.jbetterly.auto2;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+
 import com.jbetterly.auto2.R;
 import com.jbetterly.lib.FileStore;
 import com.jbetterly.lib.WebChecks;
+
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.view.View.OnClickListener;
 
 
 public class MainActivity extends Activity
 {
 
-	//GLobal Variables
+	// GLobal Variables
 	Context _context;
 	Boolean _connected = false;
+	TextView _vin;
 	TextView _makeName;
 	TextView _modelName;
 	TextView _year;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -30,9 +38,91 @@ public class MainActivity extends Activity
 
 		final Button searchButton = (Button) findViewById(R.id.goButton);
 		final EditText et = (EditText) findViewById(R.id.productField);
+		_vin = (TextView) findViewById(R.id._vin);
 		_makeName = (TextView) findViewById(R.id._makeName);
 		_modelName = (TextView) findViewById(R.id._modelName);
 		_year = (TextView) findViewById(R.id._year);
+
+		searchButton.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v)
+			{
+				// Check Network Status
+				if (WebChecks.haveNetworkConnection(MainActivity.this))
+				{
+					Log.i("NETWORK CONNECTION", "Connection Available");
+				}
+				else
+				{
+					// No Network Connection Error
+
+					Log.i("NETWORK CONNECTION", "Connection Unavailable");
+					AlertDialog.Builder alt_bld2 = new AlertDialog.Builder(
+							MainActivity.this);
+					alt_bld2.setMessage("No Network Connection Detected")
+
+							.setCancelable(false)
+							.setPositiveButton("OK",
+									new DialogInterface.OnClickListener()
+									{
+										public void onClick(
+												DialogInterface dialog, int id)
+										{
+
+										}
+									});
+
+					AlertDialog alert = alt_bld2.create();
+
+					// Title for AlertDialog
+					alert.setTitle("Alert");
+
+					// Icon for AlertDialog
+					alert.setIcon(R.drawable.ic_launcher);
+					alert.show();
+				}
+
+				Log.i("USER TYPED:", et.getText().toString());
+				getCar(et.getText().toString());
+			}
+		});
+
+		// network detection - Create Methods to Detect Network Connectivity
+
+		if (WebChecks.haveNetworkConnection(this))
+		{
+			Log.i("NETWORK CONNECTION", "Connection Established");
+		}
+		else
+		{
+
+			// No Network Connection Error
+
+			Log.i("NETWORK CONNECTION", "Connection Lost");
+			AlertDialog.Builder alt_bld = new AlertDialog.Builder(this);
+			alt_bld.setMessage("No Network Connection Detected")
+					.setCancelable(false)
+					.setPositiveButton("OK",
+							new DialogInterface.OnClickListener()
+							{
+								public void onClick(DialogInterface dialog,
+										int id)
+								{
+
+								}
+							});
+
+			AlertDialog alert = alt_bld.create();
+
+			// Title for AlertDialog
+			alert.setTitle("AlertDialogExample");
+
+			// Icon for AlertDialog
+			alert.setIcon(R.drawable.ic_launcher);
+			alert.show();
+		}
 	}
 
 	@Override
@@ -43,4 +133,10 @@ public class MainActivity extends Activity
 		return true;
 	}
 
+	
+	
+	
+	
+	
+	
 }
