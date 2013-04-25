cfd7f6c2da22f3ed7782f646cd65c96f61746385
Joel Betterly <roninmoto@gmail.com>
2013-04-25 10:28:28 -0400

    added layout to main activity

--- Java1/Week 3/src/com/jbetterly/auto2/MainActivity.java
+++ Java1/Week 3/src/com/jbetterly/auto2/MainActivity.java
@@ -2,16 +2,37 @@ package com.jbetterly.auto2;
 
 import android.os.Bundle;
 import android.app.Activity;
+import com.jbetterly.auto2.R;
+import com.jbetterly.lib.FileStore;
+import com.jbetterly.lib.WebChecks;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Context;
+
 
 public class MainActivity extends Activity
 {
 
+	//GLobal Variables
+	Context _context;
+	Boolean _connected = false;
+	TextView _makeName;
+	TextView _modelName;
+	TextView _year;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		final Button searchButton = (Button) findViewById(R.id.goButton);
+		final EditText et = (EditText) findViewById(R.id.productField);
+		_makeName = (TextView) findViewById(R.id._makeName);
+		_modelName = (TextView) findViewById(R.id._modelName);
+		_year = (TextView) findViewById(R.id._year);
 	}
 
 	@Override
