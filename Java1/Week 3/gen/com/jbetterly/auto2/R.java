58071d7ff7c87f333b62a85b3e87a8d814164419
Joel Betterly <roninmoto@gmail.com>
2013-04-25 12:23:18 -0400

    auto generated

--- Java1/Week 3/gen/com/jbetterly/auto2/R.java
+++ Java1/Week 3/gen/com/jbetterly/auto2/R.java
@@ -24,7 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int _makeName=0x7f080004;
+        public static final int _modelName=0x7f080003;
+        public static final int _vin=0x7f080005;
+        public static final int _year=0x7f080002;
+        public static final int action_settings=0x7f08000c;
+        public static final int frameLayout1=0x7f080000;
+        public static final int goButton=0x7f08000b;
+        public static final int modelLabel=0x7f080006;
+        public static final int myGrid=0x7f080001;
+        public static final int nameLabel=0x7f080007;
+        public static final int productField=0x7f08000a;
+        public static final int vinLabel=0x7f080009;
+        public static final int yearLabel=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
